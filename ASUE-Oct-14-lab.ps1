f4609f9e42ef5d26175564848c1e151383c240ef
ASUE-InfoSys2020 <113134902+ASUE-InfoSys2020@users.noreply.github.com>
2022-10-18 09:16:25 +0400

    Add files via upload

---
+++ ASUE-Oct-14-lab.ps1
@@ -0,0 +1,16 @@
+﻿# run Get-Alias command
+# save output in a file.
+# the Name of a file should contain date of its creation
+# format for the filename: [text][day-month-year].txt
+# check the created file content with notepad
+
+$myDir="C:/Users/tester/Desktop/TMP/"
+$fName='myFile_'
+$fSuffix = Get-Date -Format "%d%M%y"
+$fType = '.txt'
+Write-Host "My Filename is: $fName$fSuffix$fType"
+
+$fPath = "$myDir$fName$fSuffix$fType"
+Get-Alias|Out-File -FilePath $fPath
+
+notepad $fPath
